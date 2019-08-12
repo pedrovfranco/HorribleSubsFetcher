4f7a52949dd5f33d24736fdc7895a07383cd2e1b
Pedro Franco <yurbypedro@hotmail.com>
2019-08-12 03:16:54 +0100

    Form size changed

--- HorribleSubsFetcher/MainForm.Designer.cs
+++ HorribleSubsFetcher/MainForm.Designer.cs
@@ -197,7 +197,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 361);
+            this.ClientSize = new System.Drawing.Size(404, 310);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.priorityTextBox);
             this.Controls.Add(this.label3);
